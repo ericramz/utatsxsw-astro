fbd03e09a4d5c3b3997a2ef6b43444c966c1e912
Eric Ramirez <eric.ramirez@austin.utexas.edu>
2023-03-12 22:11:05 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import image from "@astrojs/image"
 
-import react from "@astrojs/react"
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()]
+  integrations: [tailwind(), image()]
 })
 
 import "./src/styles/global.css"
