3a4beaad32e1629390849fed826985c5925e2dc7
Eric Ramirez <eric.ramirez@austin.utexas.edu>
2023-03-16 16:29:30 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import tailwind from "@astrojs/tailwind"
 import image from "@astrojs/image"
 import react from "@astrojs/react"
 
-import netlify from "@astrojs/netlify/functions"
-
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [
     tailwind({
       config: {
@@ -16,6 +13,5 @@ export default defineConfig({
     }),
     image(),
     react()
-  ],
-  adapter: netlify()
+  ]
 })
